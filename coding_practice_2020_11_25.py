f8bc1b7ff14f7a1493fc8056f63674c90a5b5f24
sw6210 <74618983+sw6210@users.noreply.github.com>
2020-11-25 16:31:56 -0500

    Update coding_practice_2020_11_25.py

--- coding_practice_2020_11_25.py
+++ coding_practice_2020_11_25.py
@@ -1,3 +1,6 @@
+# Python Workbook If Statements
+-----------------------------
+
 # Exercise 34
 x = int(input("Enter an integer: "))
 if x % 2 == 1:
@@ -142,3 +145,61 @@ elif row % 2 == 1 and column == "a" or column == "c" or column == "e" or column 
     print("The square of the position is black.")
 else:
     print("The square of the position is white.")
+
+      
+# Codingbat String-1
+--------------------
+      
+# hello_name
+def hello_name(name):
+  return "Hello " + name + "!"
+      
+      
+# make_abba
+def make_abba(a, b):
+  return(a + b + b + a)
+      
+      
+# make_tags
+def make_tags(tag, word):
+  html =  '<' + tag + '>' + word + '</' + tag + '>'
+  return html
+	  
+
+# make_out_word
+def make_out_word(out, word):
+  return out[0:2] + word + out[2:4]
+	  
+	  
+# extra_end
+def extra_end(str):
+  end = str[-2:]
+  return end + end + end
+	  
+	  
+# first_two
+def first_two(str):
+  return str[0:2]
+
+
+# first_half
+def first_half(str):
+  half_length = len(str) / 2
+  return str[0:half_length]
+	  
+	  
+# without_end
+def without_end(str):
+  return str[1:-1]
+	  
+# combo_string
+def combo_string(a, b):
+  if len(a) > len(b):
+    return b + a + b
+  else:
+    return a + b + a
+	  
+# non_start
+def non_start(a, b):
+  return a[1:10] + b[1:10]
+	  
