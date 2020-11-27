123976cde1215638fe958a947ec13be1ebe05127
sw6210 <74618983+sw6210@users.noreply.github.com>
2020-11-27 13:54:44 -0500

    Update coding_practice_2020_11_27.py

--- coding_practice_2020_11_27.py
+++ coding_practice_2020_11_27.py
@@ -1,1 +1,73 @@
+Exercises
+---------
+Create a while loop that will print:
+    1. from 375 to 597 by 6.
+    2. from 392 to 405 by 1.
+    3. from 297 to 619 by 7.
+    4. from 310 to 838 by 11.
+    5. from 427 to 583 by 12.
+    6. from 452 to 548 by 4.
+    7. from 298 to 788 by 10.
+    8. from -448 to -160 by 8.
+    9. from -53 to 633 by 14.
+    10. from 471 to 539 by 2.
 
+1. 
+i = 375
+while i <= 597:
+    print(i)
+    i += 6
+    
+2. 
+i = 392
+while i <= 405:
+    print(i)
+    i += 1
+    
+3. 
+i = 297
+while i <= 619:
+    print(i)
+    i += 7
+    
+4.
+i = 310
+while i <= 838:
+    print(i)
+    i += 11
+    
+5. 
+i = 427
+while i <= 583:
+    print(i)
+    i += 12
+    
+6. 
+i = 452
+while i <= 548:
+    print(i)
+    i += 4
+    
+7. 
+i = 298
+while i <= 788:
+    print(i)
+    i += 10
+    
+8. 
+i = -448
+while i <= -160:
+    print(i)
+    i += 8
+    
+9. 
+i = -53
+while i <= 633:
+    print(i)
+    i += 14
+    
+10. 
+i = 471
+while i <= 539:
+    print(i)
+    i += 2
